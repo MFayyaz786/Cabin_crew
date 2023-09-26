d7e0555aef110b25c2d1afd8add1f5feb3b95732
Muhammad Fayyaz <muhammad.fayyaz@appinsnap.com>
2023-09-26 15:44:15 +0500

    latest code push to update active filter active and inactive airline,booth,flight and also api to active and inactive these

--- dist/utils/employId.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const employId = () => {
-    const employId = Math.floor(10000000 + Math.random() * 90000000);
-    return employId;
-};
-exports.default = employId;
